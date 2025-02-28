bbad1524053f24e2d93022b1d3975a96c74c6f8b
vidyasagar2110 <tvidyasagar2110@gmail.com>
2025-02-28 09:29:05 +0530

    git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import React from 'react';
 function App() {
-  return (
-  <div className="App">
- 
-  </div>
-  );
- }
- export default App;
- 
+ return (
+ <div className="App">
+ <BrowserRouter>
+ <Routes>
+ <Route
+ path="/"
+ element={<Home />}
+ />
+ </Routes>
+ </BrowserRouter>
+ </div>
+ );
+}
+export default App;
